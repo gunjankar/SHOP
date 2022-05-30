4d5781e68223137f191f9301ea47143c6551694b
gunjankar <kargunjan9@gmail.com>
2022-05-30 18:51:54 +0530

    resolved admin-site issue

---
+++ admin-site/src/redux/userRedux.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+//import { Persist } from 'react-persist';
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    currentUser: null,
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    loginStart: (state) => {
+      state.isFetching = true;
+    },
+    loginSuccess: (state, action) => {
+      state.isFetching = false;
+      state.currentUser = action.payload;
+    },
+    loginFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export default userSlice.reducer;
